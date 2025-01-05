516e657d5038682d7363196c297828c83c26c5c5
Vitaljano <=>
2025-01-06 01:47:29 +0200

    feat: add jwt create and parse methods

---
+++ backend/pkg/jwt/jwt.go
@@ -0,0 +1,53 @@
+package jwt
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWT struct {
+	Secret string
+}
+
+type JWTData struct {
+	Email string
+}
+
+func NewJwt() {
+
+}
+
+func (j *JWT) Create(data JWTData) (string, error) {
+	if len(j.Secret) == 0 {
+		return "", errors.New("JWT secret cannot be empty")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": data.Email,
+	})
+
+	t, err := token.SignedString(j.Secret)
+
+	if err != nil {
+		return "", err
+	}
+
+	return t, nil
+
+}
+
+func (j *JWT) Parse(token string) (bool, *JWTData) {
+	t, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		return []byte(j.Secret), nil
+	})
+
+	if err != nil {
+		return false, nil
+	}
+
+	email := t.Claims.(jwt.MapClaims)["email"]
+	return t.Valid, &JWTData{
+		Email: email.(string),
+	}
+
+}
