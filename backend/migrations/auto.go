cf857e41052d362ca643cf95341742d84a961eaf
Vitaljano <=>
2025-01-06 01:18:20 +0200

    feat: add user auto migration

---
+++ backend/migrations/auto.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/Vitaljano/invly/backend/internal/user"
+
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(&user.User{})
+}
